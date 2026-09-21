219f45f8b38de6e6a799365a8f5151e3b39eeb75
Yug9093 <yugpatel9093@gmail.com>
2026-09-21 14:45:06 +0530

    Revert "Practical 6"
    This reverts commit 910561ad26a883e64b512f0d327623646e884ff5.

--- task-manager-api/index.js
+++ task-manager-api/index.js
@@ -1,26 +1,22 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors');
 const loggerMiddleware = require('./middleware/logger');
 const errorHandler = require('./middleware/errorHandler');
 const taskRoutes = require('./routes/taskRoutes');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 // Database Connection
 mongoose.connect(process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/taskDB')
   .then(() => console.log('MongoDB connected successfully'))
   .catch((err) => console.error('MongoDB connection error:', err));
 
-// 1. Enable CORS for cross-origin requests
-app.use(cors());
-
-// 2. Built-in Body Parsing Middleware
+// 1. Built-in Body Parsing Middleware
 app.use(express.json());
 
-// 3. Custom Application-level Logging Middleware
+// 2. Custom Application-level Logging Middleware
 app.use(loggerMiddleware);
 
 // 3. Root Endpoint
